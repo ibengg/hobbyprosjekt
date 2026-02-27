db6fc518387d760e5efe19de3c55327ec0e34bfe
Iben Gedde <ibengg@stud.ntnu.no>
2026-02-27 16:23:01 +0100

    refactor: update SwipeCard component and styles for improved layout and consistency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import SwipeCard from "./components/swipeCard/SwipeCard";
+import SwipeGame from "./pages/SwipeGame";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -30,6 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <SwipeCard />
+      <SwipeGame />
     </>
 
   );
