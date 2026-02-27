68fb92276a6e5b9b8f9fb8fd9f24badb5f2ea4ac
Iben Gedde <ibengg@stud.ntnu.no>
2026-02-27 08:55:08 +0100

    feat: add SwipeCard component with styling

---
+++ client/src/components/swipeCard/SwipeCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SwipeCard.css";
+
+const SwipeCard: React.FC = () => {
+  return (
+    <div className="swipe-card">
+      <p className="swipe-card__text">Test question</p>
+    </div>
+  );
+};
+
+export default SwipeCard;
