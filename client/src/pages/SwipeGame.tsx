8b8564006dbe5310b8533882f3be25a2b9b42b2f
Iben Gedde <ibengg@stud.ntnu.no>
2026-02-27 21:19:33 +0100

    feat: implement navigation buttons and question counter in SwipeGame component

--- client/src/pages/SwipeGame.tsx
+++ client/src/pages/SwipeGame.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import SwipeCard from "../components/swipeCard/SwipeCard";
 import "./swipeGame.css";
 
+const QUESTIONS = [
+  "Hvem i rommet er mest sannsynlig å snuse på do?",
+  "Hvem har tatt flest bokser på én uke?",
+  "Hvem gjemmer snusen sin for foreldrene?",
+  "Hvem ville klart seg lengst uten snus på ferie?",
+  "Hvem introduserte flest venner til snus?",
+  "Hvem er mest sannsynlig å ha snus på bryllupsdagen sin?",
+  "Hvem bytter smak oftest?",
+  "Hvem er mest defensiv når noen kritiserer snusvanene deres?",
+];
+
 const SwipeGame: React.FC = () => {
+  const [index, setIndex] = useState(0);
+
+  const isFirst = index === 0;
+  const isLast = index === QUESTIONS.length - 1;
+
   return (
     <main className="swipe-game">
       <header className="swipe-game__header">
@@ -11,8 +27,30 @@ const SwipeGame: React.FC = () => {
       </header>
 
       <section className="swipe-game__card-area">
-        <SwipeCard />
+        <button
+          className="swipe-game__arrow swipe-game__arrow--left"
+          onClick={() => setIndex((i) => i - 1)}
+          disabled={isFirst}
+          aria-label="Previous question"
+        >
+          &#8592;
+        </button>
+
+        <SwipeCard question={QUESTIONS[index]} />
+
+        <button
+          className="swipe-game__arrow swipe-game__arrow--right"
+          onClick={() => setIndex((i) => i + 1)}
+          disabled={isLast}
+          aria-label="Next question"
+        >
+          &#8594;
+        </button>
       </section>
+
+      <p className="swipe-game__counter">
+        {index + 1} / {QUESTIONS.length}
+      </p>
     </main>
   );
 };
