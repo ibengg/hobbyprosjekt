9a5634e60ef78134ec59f16d134edf7f700083fe
Iben Gedde <ibengg@stud.ntnu.no>
2026-02-28 10:21:16 +0100

    feat: update import paths for SwipeGame, HomePage and JeopardyBoard components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SwipeGame from "./pages/SwipeGame.tsx";
-import HomePage from "./pages/HomePage.tsx";
+import SwipeGame from "./pages/swipeGame/SwipeGame.tsx";
+import HomePage from "./pages/homePage/HomePage.tsx";
+import JeopardyBoard from "./pages/jeopardyBoard/JeopardyBoard.tsx";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/snusboksleken" element={<SwipeGame />} />
         <Route path="/game" element={<SwipeGame />} />
+        <Route path="/jeopardy" element={<JeopardyBoard />} />
       </Routes>
     </BrowserRouter>
   );
